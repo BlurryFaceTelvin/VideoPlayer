f1cd432b76ecab6def881a519cda24846c9f3dd6
Telvin Njoroge <tnjoroge95@gmail.com>
2017-10-05 08:59:19 +0300

    Second Commit

--- app/src/main/java/com/example/blurryface/videoplayer/VideoPlayerActivity.java
+++ app/src/main/java/com/example/blurryface/videoplayer/VideoPlayerActivity.java
@@ -1,12 +1,15 @@
 package com.example.blurryface.videoplayer;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
 public class VideoPlayerActivity extends AppCompatActivity {
     VideoView videoView;
+    String TAG = "VideoPlayer";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,12 @@ public class VideoPlayerActivity extends AppCompatActivity {
         MediaController mediaController = new MediaController(this);
         mediaController.setAnchorView(videoView);
         videoView.setMediaController(mediaController);
+        videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                Log.i(TAG,"Duration ="+videoView.getDuration());
+            }
+        });
         videoView.start();
     }
 }
